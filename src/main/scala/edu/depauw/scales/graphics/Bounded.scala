92942bb4bdd1c34f4c3a4169bc35902a906a5b66
Brian Howard <bhoward@depauw.edu>
2014-07-29 15:06:52 -0400

    Integration!

--- src/main/scala/edu/depauw/scales/graphics/Bounded.scala
+++
@@ -1,7 +0,0 @@
-package edu.depauw.scales.graphics
-
-import Base._
-
-case class Bounded(graphic: Graphic, bounds: Bounds) extends Graphic {
-  def render(ctx: GraphicsContext): Unit = graphic.render(ctx)
-}
